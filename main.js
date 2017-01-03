fe2f3218d766e9752b53b0ccd4f05cf0c4a73d17
Michael Pimlott <pettingbutterscotch@gmail.com>
2017-01-03 14:17:52 -0800

    add data types, arrays, functions

---
+++ main.js
@@ -0,0 +1,139 @@
+/*variables*/
+
+var message;
+var items = ["hammer",
+            "screwdriver",
+            "nails",
+            "axe",
+            "wrench",
+            "pick",
+            "gloves",
+            "knife",
+            "pliers",
+            "tape"];
+
+var tools = [
+  {
+    name: "hammer",
+    cost: 9.99,
+    id: 101,
+    img: "hammer.jpg"
+  },
+  {
+    name: "screwdriver",
+    cost: 10.99,
+    id: 102,
+    img: "screwdriver.jpg"
+  },
+  {
+    name: "nails",
+    cost: 1.99,
+    id: 103,
+    img: "nails.jpg"
+  },
+  {
+    name: "axe",
+    cost: 14.99,
+    id: 104,
+    img: "axe.jpg"
+  },
+  {
+    name: "wrench",
+    cost: 7.99,
+    id: 105,
+    img: "wrench.jpg"
+  },
+  {
+    name: "pick",
+    cost: 19.99,
+    id: 106,
+    img: "pick.jpg"
+  }
+
+];
+
+var messanger = {};
+
+var workout = {
+    chest: "pushup",
+    booty: "squat",
+    arms: "curl",
+
+    /*using the keyword this to access methods of the workout object*/
+
+     workoutPrompt: function() {
+      console.log(this.chest + " " + this.booty);
+    }
+
+};
+
+
+/*--------------------------------*/
+/* add items to end of items array*/
+items.push("paintbrush", "ladder");
+/*sort items a-z. check documentation for other sort options*/
+items.sort();
+
+
+/*changing variable values*/
+message = "hello, world!";
+
+console.log(message);
+
+message = "I fucked up";
+
+console.log(message);
+
+/*-----------------------------*/
+
+/*random number function*/
+
+function randomInt(max) {
+  var rand = Math.random() * max;
+  rand = Math.round(rand);
+  return rand;
+}
+
+console.log(randomInt(100));
+
+/*-----------------------------*/
+
+/*array for loop for items*/
+
+function listItems() {
+for(var i=0; i<items.length; i++){
+  console.log(items[i]);
+}
+};
+
+/*---------------------------------*/
+
+/*object for loop for tools*/
+
+function listTools() {
+  for(var i=0; i<tools.length; i++){
+    console.log(tools[i].name + " is $ " + tools[i].cost);
+  }
+};
+/*---------------------------------*/
+
+/*declaring object method for empty object messanger*/
+
+messanger.sayBurrito = function() {
+  console.log('Burrito!');
+
+};
+
+/*---------------------------------*/
+
+/*calling functions*/
+listItems();
+listTools();
+messanger.sayBurrito();
+/*calling inner function from workout object*/
+workout.workoutPrompt();
+/*change properties on workout object*/
+workout.chest="bench press";
+workout.booty="lunge";
+/*call function again*/
+workout.workoutPrompt();
