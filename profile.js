3b247a70646cc6329878539a5900e7ac1e0c5ae6
Michael Pimlott <pettingbutterscotch@gmail.com>
2017-01-05 14:38:15 -0800

    declare variables in profile.js

---
+++ profile.js
@@ -0,0 +1,10 @@
+/*1)Decide on and create some variables*/
+
+/*The template data*/
+var template = document.getElementById("template").innerHTML;
+/*The div that holds the form*/
+var form = document.getElementById("form");
+/*The empty div where we will put the preview*/
+var preview = document.getElementById("preview");
+/*The preview button*/
+var previewBtn = document.getElementById("previewBtn");
